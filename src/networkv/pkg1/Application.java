101e8449980213b519c31629887b3b61535a6eed
Morten Solli <morten-solli@gmail.com>
2016-03-10 16:01:14 +0100

    8.1-8.3

--- src/networkv/pkg1/Application.java
+++ src/networkv/pkg1/Application.java
@@ -1,16 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package networkv.pkg1;
 
 /**
- *
+ * Main class of tthe NetworkV-1. application.
+ * 
  * @author Morten
  */
 public class Application
 {
+    private NewsFeed newsFeed;
+    private MessagePost messagePost1;
+    private MessagePost messagePost2;
+    private PhotoPost photoPost1;
+    private PhotoPost photoPost2;
     
     
     /**
@@ -18,8 +20,9 @@ public class Application
      */
     public Application()
     {
-       
-        
+       newsFeed = new NewsFeed();
+       makeDummies();
+       display();
     }
     
     /**
@@ -31,9 +34,37 @@ public class Application
         
     }
     
-    
-    public void makeDummies()
+    /**
+     * Fills the newsfeed with dummies
+     */
+    private void makeDummies()
     {
+        //Making 2 message posts and 2 photo posts.
+        messagePost1 = new MessagePost("kniven","Jeg er skarp idag");
+        messagePost2 = new MessagePost("Oscar", "I dag er livet verdt å leve");
+        photoPost1 = new PhotoPost("Eirik", "sol.jpg", "Sola skinner fint idag");
+        photoPost2 = new PhotoPost("Ole Martin", "noSkjeg.jpg", "Tatt skjegget");
+       
+        //Adding posts to the newsfeed
+        newsFeed.addMessagePost(messagePost1);
+        newsFeed.addMessagePost(messagePost2);
+        newsFeed.addPhotoPost(photoPost1);
+        newsFeed.addPhotoPost(photoPost2);
+        messagePost1.addComment("Skarping as");
+        messagePost2.addComment("Like, Betyr likar");
+        messagePost2.addComment("Så flott");
+        photoPost1.addComment("Deilig med sol");
+        photoPost2.addComment("Neeeeeeeeeeeeeeeeei! :'(");
+                
+    }
+    
+    /**
+     * Display the information in the newsfeed and the comments
+     */
+    private void display()
+    {
+        //Displaying the posts
+        newsFeed.show();
         
     }
     
--- src/networkv/pkg1/MessagePost.java
+++ src/networkv/pkg1/MessagePost.java
@@ -1,3 +1,4 @@
+package networkv.pkg1;
 import java.util.ArrayList;
 
 /**
